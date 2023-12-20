b5c302d73e957a67f842adcb15d914b730f54982
zkrzn <15352294+zkrzn@users.noreply.github.com>
2023-12-20 20:59:44 +0100

    update

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -28,7 +28,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://i.ibb.co/0mHr8gP/robot.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://i.ibb.co/Wft3367/directeur.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://i.ibb.co/Wft3367/directeur.png">
+        <img src="https://i.ibb.co/0mHr8gP/robot.png ">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
